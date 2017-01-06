43cb989b37e512f508cfcf6156ca31869ab91daf
kriswans <kris@swannynet.com>
2017-01-06 12:04:12 -0500

    fixed the skewed index

--- HostListBuilder.py
+++ HostListBuilder.py
@@ -14,8 +14,8 @@ def RangePrinter(prefix,lo_h,hi_h):
     host_index=0
     hosts=range(lo_h,hi_h)
     for addys in hosts:
+      str_addys=str(addys)
       host_index+=1
-      str_addys=str(1+addys)
       ip_addr=prefix+'.'+str_addys
       print(host_index, ip_addr)
       ip_file=open('hosts_list','a')
@@ -23,3 +23,5 @@ def RangePrinter(prefix,lo_h,hi_h):
       ip_file.write('\n')
       ip_file.close()
   except ValueError: print('upper and lower bounds require numerical value')
+
+#RangePrinter(prefix,lo_h,hi_h)
