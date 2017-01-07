2b129f42aaa7f6c2f2a48afd9cd5834ef75d7eb5
kriswans <kris@swannynet.com>
2017-01-07 10:27:41 -0500

    Exception handling improvements
    When pullinvwrite.HostsCreateList() is called, erroneous list elements
    are removed and file output continues. Problem still exists after list
    is exhausted.

--- pullinvwrite.py
+++ pullinvwrite.py
@@ -3,11 +3,13 @@
 #It will do this in a single row, with the hostname and IP in the first 2 columns
 #use: pip install -e git+https://github.com/paramiko/paramiko/#egg=paramiko to install paramiko for ssh connectivity
 
+import sys
 import paramiko
+from contextlib import suppress
+import datetime
+import time
 
-#host='192.168.220.238'
-#username='cisco'
-#password='cisco'
+line_do=[]
 
 def  show_inventory_raw(host):
 
@@ -56,20 +58,56 @@ def  show_hostname(host):
         cmdout.close()
   ssh.close()
 
-def pullDataFromFile():
-    line_do=[]
+
+def HostsCreateList():
+    ## make sure to set line_do=[] outside of this function and before it's called in the program
     with open('hosts_list','r') as h_l:
-        for h_rows in h_l:
-            h_str_row=h_rows.strip()
+      for h_rows in h_l:
+        h_str_row=h_rows.strip()
             #h_str_row="'"+str(h_str_row)+"'"
             #print(h_str_row)
-            line_do.append(h_str_row)
-
-    host_list_len=(len(line_do))
-    h_i=0
+        line_do.append(h_str_row)
+      host_list_len=(len(line_do))
+      h_i=0
     while h_i < host_list_len :
-      show_hostname(line_do[h_i])
-      show_inventory_raw(line_do[h_i])
-      h_i+=1
+        try:
+          show_hostname(line_do[h_i])
+          show_inventory_raw(line_do[h_i])
+          h_i+=1
+        except Exception:
+              #print("problem with IP addess")
+          print("there was a problem connecting ssh to: {ld}".format(ld=line_do[h_i]))
+          ts = time.time()
+          st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+          error_file=open('error_file.txt','a')
+          error_file.write("{timestp}||>there was a problem connecting ssh to: {ld} at {timestp}\n".format(ld=line_do[h_i], timestp=st))
+          error_file.close()
+          rm_line=line_do[h_i]
+          line_do.remove(rm_line)
+          host_list_len=len(line_do)
+          LoopThruHosts(h_i,host_list_len)
 
-#pullDataFromFile()
+
+
+def LoopThruHosts(h_i,host_list_len):
+          while h_i <= host_list_len :
+            try:
+              show_hostname(line_do[h_i])
+              show_inventory_raw(line_do[h_i])
+              h_i+=1
+            except Exception:
+              print("there was a problem connecting ssh to: {ld}".format(ld=line_do[h_i]))
+              ts = time.time()
+              st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+              error_file=open('error_file.txt','a')
+              error_file.write("{timestp}||>there was a problem connecting ssh to: {ld} at {timestp}\n".format(ld=line_do[h_i], timestp=st))
+              error_file.close()
+              rm_line=line_do[h_i]
+              line_do.remove(rm_line)
+              host_list_len=len(line_do)
+              print(host_list_len)
+              print(h_i)
+              LoopThruHosts(h_i,host_list_len)
+
+
+#HostsCreateList()
